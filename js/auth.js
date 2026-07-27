80daa22f0514da9be6ee931b5a6f793156da4457
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-27 16:59:13 +0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,52 @@
- 
+import { auth } from "../firebase.js";
+
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+// SIGN UP
+const signupBtn = document.getElementById("signupBtn");
+
+if (signupBtn) {
+  signupBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      alert("Account Created Successfully!");
+
+      window.location.href = "login.html";
+
+    } catch (error) {
+      alert(error.message);
+    }
+
+  });
+}
+
+// LOGIN
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+
+      alert("Login Successful!");
+
+      window.location.href = "index.html";
+
+    } catch (error) {
+      alert(error.message);
+    }
+
+  });
+} 
